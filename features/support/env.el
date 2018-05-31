35209d39508204bb4bbaad437f3703432f69f72b
kyoncho <yyoncho@gmail.com>
2018-05-31 23:05:23 +0300

    Increase request timeout

--- features/support/env.el
+++ features/support/env.el
@@ -33,7 +33,8 @@
  (require 'lsp-java)
  (setq lsp-java-workspace-dir (f-join lsp-java-test-root "workspace")
        lsp-java-workspace-cache-dir (f-join lsp-java-test-root "workspace-cache")
-       lsp-java-server-install-dir (locate-user-emacs-file "eclipse.jdt.ls/server/"))
+       lsp-java-server-install-dir (locate-user-emacs-file "eclipse.jdt.ls/server/")
+       lsp-response-timeout 30)
  (when (file-exists-p lsp-java-test-root)
    (delete-directory lsp-java-test-root t)))
 
