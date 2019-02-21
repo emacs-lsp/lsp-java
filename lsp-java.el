af35486baf1a246b329e871ee97fc69e68d3f3ac
Marcus Geiger <git010@teufelsgraben.xyz>
2019-02-21 09:58:26 +0100

    Fix comment formatting

--- lsp-java.el
+++ lsp-java.el
@@ -18,7 +18,9 @@
 ;; You should have received a copy of the GNU General Public License
 ;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-;;; Commentary: Java specific adapter for LSP mode
+;;; Commentary:
+
+;; Java specific adapter for LSP mode
 
 ;;; Code:
 (require 'cc-mode)
