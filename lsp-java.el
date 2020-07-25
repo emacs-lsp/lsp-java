fbd04cff52521f393408a219125d3a0727705045
Ivan Yonchovski <yyoncho@gmail.com>
2020-07-25 21:09:30 +0300

    Move dap-java into lsp-java

--- lsp-java.el
+++ lsp-java.el
@@ -2,7 +2,7 @@
 
 ;; Version: 3.0
 
-;; Package-Requires: ((emacs "25.1") (lsp-mode "6.0") (markdown-mode "2.3") (dash "2.14.1") (f "0.20.0") (ht "2.0") (dash-functional "1.2.0") (request "0.3.0") (treemacs "2.5"))
+;; Package-Requires: ((emacs "25.1") (lsp-mode "6.0") (markdown-mode "2.3") (dash "2.14.1") (f "0.20.0") (ht "2.0") (dash-functional "1.2.0") (request "0.3.0") (treemacs "2.5") (dap-mode "0.5"))
 ;; Keywords: languague, tools
 ;; URL: https://github.com/emacs-lsp/lsp-java
 
@@ -1547,8 +1547,6 @@ current symbol."
      :mode 'tick)))
 
 
-
-;;;###autoload(with-eval-after-load 'lsp-mode (require 'lsp-java))
 
 (provide 'lsp-java)
 ;;; lsp-java.el ends here
