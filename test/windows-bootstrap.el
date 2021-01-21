7f39da491a65205fa33acc855d366088308ad69d
Jen-Chieh <jcs090218@gmail.com>
2021-01-21 13:52:52 +0800

    Update dependency list

--- test/windows-bootstrap.el
+++ test/windows-bootstrap.el
@@ -31,7 +31,7 @@
 
 (let* ((package-archives '(("melpa" . "https://melpa.org/packages/")
                            ("gnu" . "https://elpa.gnu.org/packages/")))
-       (pkgs '(dash dash-functional f lv ht spinner markdown-mode deferred)))
+       (pkgs '(lsp-mode markdown-mode dash f ht dash-functional request treemacs dap-mode)))
   (package-initialize)
   (package-refresh-contents)
 
