b7db5b38b0ea1018506d7b71b861354826e9e597
Blue Ye <yejianfengblue@foxmail.com>
2021-11-09 14:32:43 +0800

    Bump jdtls to 1.5.0 (#363)
    * Bump download-maven-plugin to 1.6.7
    
    Https proxy works after 1.5.1. See https://github.com/maven-download-plugin/maven-download-plugin/pull/157
    
    * Bump jdtls to 1.5.0
    
    Fix https://github.com/emacs-lsp/lsp-java/issues/362

--- lsp-java.el
+++ lsp-java.el
@@ -47,7 +47,7 @@ The slash is expected at the end."
   :risky t
   :type 'directory)
 
-(defcustom lsp-java-jdt-download-url "https://download.eclipse.org/jdtls/milestones/1.4.0/jdt-language-server-1.4.0-202109161824.tar.gz"
+(defcustom lsp-java-jdt-download-url "https://download.eclipse.org/jdtls/milestones/1.5.0/jdt-language-server-1.5.0-202110191539.tar.gz"
   "JDT JS download url.
 Use http://download.eclipse.org/che/che-ls-jdt/snapshots/che-jdt-language-server-latest.tar.gz if you want to use Eclipse Che JDT LS."
   :group 'lsp-java
